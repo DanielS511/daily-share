433e3cb47cb3cff525192fe537c4765059e978f3
Mingqian Sun <47837369+DanielS511@users.noreply.github.com>
2021-08-10 15:58:15 +0800

    Create xml file for one post in post_item.xml Create adapter for posts in PostsAdapter.kt Implement UI for posts in PostActivity.kt

--- app/src/main/java/com/example/dailyshare/PostActivity.kt
+++ app/src/main/java/com/example/dailyshare/PostActivity.kt
@@ -1,12 +1,13 @@
 package com.example.dailyshare
 
-import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.dailyshare.models.Post
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
@@ -15,12 +16,30 @@ import com.google.firebase.ktx.Firebase
 
 class PostActivity : AppCompatActivity() {
     private lateinit var firestoreDB: FirebaseFirestore
+    private lateinit var posts : MutableList<Post>
+    private lateinit var adapter: PostsAdapter
+    private lateinit var rvPosts : RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
+
         firestoreDB =  Firebase.firestore
-        var postsReference = firestoreDB
+
+        //Set the list of posts
+        posts = mutableListOf()
+        //Set the adapter
+        adapter = PostsAdapter(this, posts)
+
+        //Bind the adapter and layout manager with the recycleView
+        rvPosts = findViewById(R.id.rvPosts)
+        rvPosts.adapter = adapter
+        rvPosts.layoutManager = LinearLayoutManager(this)
+
+        /*
+        get posts from database
+         */
+        val postsReference = firestoreDB
             .collection("posts")
             .limit(40)
             .orderBy("creation_time_ms", Query.Direction.DESCENDING)
@@ -31,8 +50,16 @@ class PostActivity : AppCompatActivity() {
                 return@addSnapshotListener
             }
             val postList = value.toObjects(Post::class.java)
+
+            //clear exist items in posts and add data acquired to posts
+            posts.clear()
+            posts.addAll(postList)
+
+            //update the recyclerView
+            adapter.notifyDataSetChanged()
+
             for (post in postList){
-                Log.i("PostActivity", "Post ${post}")
+                Log.i("PostActivity", "Post $post")
             }
         }
     }
---
+++ app/src/main/java/com/example/dailyshare/PostsAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.dailyshare
+/*
+This is the adapter to make posts appear on the recycleView
+A post as a view is represent by post_item.xml
+ */
+
+import android.content.Context
+import android.text.format.DateUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.dailyshare.models.Post
+
+class PostsAdapter (val context: Context, val posts: List<Post>)
+    : RecyclerView.Adapter<PostsAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        private val tvName : TextView = itemView.findViewById(R.id.tvName)
+        private val tvDescription : TextView = itemView.findViewById(R.id.tvDescription)
+        private val tvRelativeTime : TextView = itemView.findViewById(R.id.tvRelativeTime)
+        private val ivPicture : ImageView = itemView.findViewById(R.id.ivPicture)
+        fun bind(post: Post) {
+            tvName.text = post.user?.username
+            tvDescription.text = post.description
+            Glide.with(context).load(post.imageUrl).into(ivPicture)
+            tvRelativeTime.text = DateUtils.getRelativeTimeSpanString(post.creationTimeMs)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.post_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(posts[position])
+    }
+
+    override fun getItemCount(): Int {
+        return posts.size
+    }
+}
